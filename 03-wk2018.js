8c73b21e27c4c6275680d2e81bc16e9f806b6f17
MichaelVilvoorde <baeyensmichael@hotmail.com>
2020-03-02 10:28:26 +0100

    added oefening 2

---
+++ 03-wk2018.js
@@ -0,0 +1,20 @@
+// function toonWinnaars(input) {
+//     var landen = new Array("Frankrijk", "Kroatië", "Beglië");
+//     document.write("Op plaats 1 staat : " + input);
+//     document.write("Op plaats 1 staat : " + input);
+//     document.write("Op plaats 1 staat : " + input);
+// }
+
+
+
+
+
+function groet(input) {
+    namen = new Array("An", "Fred", "Anne", "Alfred");
+    document.write("<br>" + "Goedendag, " + input);
+    document.write("<br>" + "Alles goed met de kinderen " + input + "?" + "<br>");
+}
+groet(namen[0]);
+groet(namen[1]);
+groet(namen[2]);
+groet(namen[3]);
