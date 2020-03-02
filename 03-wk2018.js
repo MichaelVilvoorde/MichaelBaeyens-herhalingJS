7c64343dd4c9668e35f7d3443ff15c34c4ddcc9b
MichaelVilvoorde <baeyensmichael@hotmail.com>
2020-03-02 15:37:27 +0100

    .

--- 03-wk2018.js
+++ 03-wk2018.js
@@ -1,13 +1,22 @@
-var landen = new Array("Frankrijk", "Kroatië", "Beglië");
+// var landen = new Array("Frankrijk", "Kroatië", "Beglië");
 
-function toonWinnaars1(input) {
-    document.write("<br>Op plaats 1 staat : " + input + "." + "<br>");
-}
+// function toonWinnaars1(input) {
+//     document.write("<br>Op plaats 1 staat : " + input + "." + "<br>");
+// }
 
-function toonWinnaars2(input) {
-    document.write("<br>Op plaats 2 staat : " + input + "." + "<br>");
-}
+// function toonWinnaars2(input) {
+//     document.write("<br>Op plaats 2 staat : " + input + "." + "<br>");
+// }
 
-function toonWinnaars3(input) {
-    document.write("<br>Op plaats 3 staat : " + input + ".");
+// function toonWinnaars3(input) {
+//     document.write("<br>Op plaats 3 staat : " + input + ".");
+// }
+
+function toonWinnaars() {
+    var winnaars = ["Frankrijk", "Kroatië", "België"];
+    var lengte = winnaars.length;
+    for (var i = 0; i < lengte; i++) {
+        document.write("<p>Op plaats " + (i + 1) + " staat : " + winnaars[i] + ".</p>")
+    }
 }
+toonWinnaars();
